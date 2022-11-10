2eb6a260ab08a0243a7f9a9c0b5c996edf3a5697
Loose37 <70845110+Loose37@users.noreply.github.com>
2022-11-11 00:42:12 +0900

    feat:configure migrations and creates first migration

---
+++ db/migrations/20221110152547_create_students_table.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("students", (table) => {
+    table.increments("id");
+    table.string("first_name", 255)
+    .notNullable();
+    table.string("last_name", 255)
+    .notNullable();
+    table.string("image", 255)
+    .notNullable();
+    table.string("student_unique_ID", 255)
+    .notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("students")
+  
+};
