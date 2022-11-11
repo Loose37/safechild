432ef9ca405e89d8105f3855ff382184a6db6eae
Loose37 <70845110+Loose37@users.noreply.github.com>
2022-11-12 01:25:50 +0900

    feat:implement /testdb endpoint to fetch all students

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const db = require("../db/knex")
 
 function setupServer () {
   const app = express();
@@ -10,11 +11,22 @@ function setupServer () {
   app.get("/", (req, res) => {
     res.status(200).send("this is your test homepage");
   });
+  
+  
+  app.get("/testdb", async (req, res) => {
+    try{
+      const students = await db("students")
+      .select("*")
+      .timeout(1500);
+      students.length > 0
+      ? res.status(200).send(students)
+      : res.status(404).send("no students found");
+    }catch(err){
+      res.status(500).send(err);
+    }
+  });
 
 
-  
-  
-  
   return app;
 }
 
