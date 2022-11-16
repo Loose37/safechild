fac3fe07d1df74161502ee40b7dc829cab064f12
Loose37 <70845110+Loose37@users.noreply.github.com>
2022-11-16 12:23:47 +0900

    feat:forgot to change branches.

--- client/src/components/Parentspage.js
+++ client/src/components/Parentspage.js
@@ -1,27 +1,40 @@
 import React, { useState,useEffect } from 'react';
 import {auth} from "../firebase-config";
 import {onAuthStateChanged} from "firebase/auth"
+import {useNavigate} from "react-router-dom"
 
 
 export function Parentspage (props) {
- const {user,setUser} = props;
-
+  const {user,setUser} = props;
+  const [history,setHistory] = useState([])
+  const current = new Date();  
+  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+  const navigate = useNavigate()
 
  useEffect(() => {
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
-  
   })
 },[]);
 
   
 
+
   return(
     <div className="parents_page">
-      <h1>This is the Parents Page</h1>
+      <h1>Welcome {user.email}</h1>
+      <h3>{date}</h3>
+      <button onClick={(e)=>{
+        navigate("/")
+        }}>Back to Login Page</button>
 
+      <div className='events_history'>
+
+      </div>
+
+      <button className='refresh_history'>Click here to refresh your child's history</button>
       
-     
+  
 
     </div>
 
